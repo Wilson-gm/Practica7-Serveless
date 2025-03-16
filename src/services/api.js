b104f8cfe55d3bc1c498204abc63e02960fc5a20
Wilson-gm <wdgb0001@ce.pucmm.edu.do>
2025-03-16 17:24:44 -0400

    cambios

--- src/services/api.js
+++ src/services/api.js
@@ -1,18 +1,25 @@
 import axios from "axios";
 
-const API_URL = "https://tu-api.com"; // Reemplazar con URL real
+const API_URL = "https://g41qgto6o9.execute-api.us-east-1.amazonaws.com/"; // Reemplazar con la URL correcta
+
+const axiosInstance = axios.create({
+  baseURL: API_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
 
 export const obtenerReservasActivas = async () => {
-  const res = await axios.get(`${API_URL}/reservas`);
+  const res = await axiosInstance.get(`/reservas`);
   return res.data;
 };
 
 export const crearReserva = async (reserva) => {
-  const res = await axios.post(`${API_URL}/reservas`, reserva);
+  const res = await axiosInstance.post(`/reservas`, reserva);
   return res.data;
 };
 
 export const eliminarReserva = async (id) => {
-  const res = await axios.delete(`${API_URL}/reservas`, { data: { id } });
+  const res = await axiosInstance.delete(`/reservas`, { data: { id } });
   return res.data;
 };
