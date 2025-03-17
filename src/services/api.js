ee1697fb6fd5e7f50d1decbf5f93b8692b1f37db
Wilson-gm <wdgb0001@ce.pucmm.edu.do>
2025-03-17 15:53:59 -0400

    nueva url

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "https://gy4mbvgbdk.execute-api.us-east-1.amazonaws.com/"; // 🚨 Verifica la URL
+const API_URL = "https://urps9eub47.execute-api.us-east-1.amazonaws.com/"; // 🚨 Verifica la URL
 
 const axiosInstance = axios.create({
   baseURL: API_URL,
